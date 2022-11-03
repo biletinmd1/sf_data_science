72d30803c9e865888e5e5eed3b352ef2259940ca
Biletinmd <biletinmd1@gmail.com>
2022-11-03 23:00:17 +0300

    final

--- final_task/game_v2.py
+++ final_task/game_v2.py
@@ -1,31 +1,35 @@
 """Игра угадай число
 Компьютер сам загадывает и сам угадывает число
 """
-
+import random
 import numpy as np
 
 
 def random_predict(number: int = 1) -> int:
-    """Рандомно угадываем число
+    """_summary_
 
     Args:
-        number (int, optional): Загаданное число. Defaults to 1.
+        number (int, optional): _description_. Defaults to np.random.randint(1, 101).
 
     Returns:
-        int: Число попыток
+        int: _description_
     """
     count = 0
-    big_number = 101
-    low_number = 1
-    number = np.random.randint(1, 101)
-
+    nmax = 102
+    nmin = 1
+    
     while True:
         count += 1
-        mid1 = round((big_number + low_number)/2)  # предполагаемое число
-        if count > 18: break  # выход из цикла если угадали
+        predict_number = (nmin+nmax)//2# предполагаемое число
+        if number < predict_number:
+            nmax = predict_number
+        elif number > predict_number:
+            nmin = predict_number
+        else:
+            number == predict_number
+            # print(f'Компьютер угадал загаданное число за {count} попыток. Это число {number}')
+            break
     return count
-
-
 def score_game(random_predict) -> int:
     """За какое количство попыток в среднем за 1000 подходов угадывает наш алгоритм
 
